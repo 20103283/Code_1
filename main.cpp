2d58cb51e7735321037179f1686f8dd4bae82ebc
20103283 <110708291+20103283@users.noreply.github.com>
2022-08-06 10:20:30 +0530

    First commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int Sum(int a, int b){
+    return a+b;
+}
+
+int main()
+{
+    int num1=34;
+    int num2=45;
+    cout<<Sum(num1, num2);
+    
+    return 0;
+}
